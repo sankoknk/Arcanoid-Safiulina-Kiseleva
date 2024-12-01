f3522487a33ab633237395a87163a722cd4a8705
sankoknk <sam.sebe.krouly@gmail.com>
2024-12-01 16:44:28 +0300

    done: платформы в форме класса TODO: описать ball/plblock классом, задать условия исчезновения платформ TODO: меню, оформление, расположение платформ

--- fullcode.py
+++ fullcode.py
@@ -19,6 +19,21 @@ plblock_h = 30
 plblock = pygame.Rect(screen_w // 2 - plblock_w // 2, screen_h - plblock_h - 60, plblock_w, plblock_h)
 plblock_speed = 8
 
+class Platform:
+    def __init__(self, x, y):
+        self.platform_w = 75
+        self.platform_h = 25
+        self.rect = pygame.Rect(x, y, self.platform_w, self.platform_h)
+
+    def render(self):
+        pygame.draw.rect(screen, (255, 160, 122), self.rect)
+
+
+platforms = []
+for x in range(25, screen_w - 75, 85):
+    for y in range(30, screen_h // 2, 35):
+        platforms.append(Platform(x, y))
+
 # ПАРАМЕТРЫ МЯЧИКА
 ball_r = 40
 ball = pygame.Rect(screen_w // 2 - ball_r // 2, screen_h - ball_r - plblock_h - 60, ball_r, ball_r)
@@ -52,8 +67,9 @@ while True:
     if ball.colliderect(plblock) and dir_y > 0:
         dir_y = - dir_y
 
-
     screen.fill((0, 0, 0))
+    for platform in platforms:
+        pygame.draw.rect(screen, (139, 0, 0), platform)
     pygame.draw.rect(screen, (139, 0, 0), plblock)
     pygame.draw.rect(screen, (178, 34, 34), ball)
     pygame.display.flip()
