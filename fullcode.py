d348d87bbe38af57ec8942cf61385e610965069c
safiulinaal <salfiullina.a.a@gmail.com>
2024-12-10 03:13:52 +0300

    небольшие изменения - важные глобальные переменные игры капсом - функция liesbetween переименована и перемещена ближе к месту использования - убраны неиспользованные импорты - небольшие переименования (например ball_r в radius) - операторы декремента/инкремента (-=, +=...)

--- fullcode.py
+++ fullcode.py
@@ -1,29 +1,22 @@
-import random
 import pygame
-import sys
-import math
 
 pygame.init()
 
 # ПАРАМЕТРЫ ЭКРАНА
-screen_w = 600
-screen_h = 800
-screen = pygame.display.set_mode((screen_w, screen_h))
+SCREEN_W = 600
+SCREEN_H = 800
+SCREEN = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 pygame.display.set_caption('Arcanoid')
 
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Calibri", 16)
-
-
-# image = pygame.image.load("image1.jpg")
-# image = pygame.transform.scale(image, (screen_w, screen_h))
+CLOCK = pygame.time.Clock()
+FONT = pygame.font.SysFont("Calibri", 16)
 
 
 class Plblock:
     def __init__(self):  # ПАРАМЕТРЫ ИГРОВОГО БЛОКА
         self.plblock_w = 150
         self.plblock_h = 15
-        self.rect = pygame.Rect(screen_w // 2 - self.plblock_w // 2, screen_h - self.plblock_h - 60, self.plblock_w,
+        self.rect = pygame.Rect(SCREEN_W // 2 - self.plblock_w // 2, SCREEN_H - self.plblock_h - 60, self.plblock_w,
                                 self.plblock_h)
         self.plblock_speed = 6
 
@@ -31,14 +24,12 @@ class Plblock:
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.x > 0:
             self.rect.x -= self.plblock_speed
-        if key[pygame.K_RIGHT] and self.rect.x < screen_w - self.plblock_w:
+        if key[pygame.K_RIGHT] and self.rect.x < SCREEN_W - self.plblock_w:
             self.rect.x += self.plblock_speed
 
     def render(self):  # СОЗДАНИЕ БЛОКА
-        pygame.draw.rect(screen, (139, 0, 0), self.rect)
-
-
-
+        self.update()
+        pygame.draw.rect(SCREEN, (139, 0, 0), self.rect)
 
 
 class Platform:
@@ -46,25 +37,23 @@ class Platform:
         self.platform_w = 60
         self.platform_h = 20
         self.rect = pygame.Rect(x, y, self.platform_w, self.platform_h)
+
         self.vec_topright = pygame.Vector2(self.rect.topright) - pygame.Vector2(self.rect.center)
         self.vec_bottomright = pygame.Vector2(self.rect.bottomright) - pygame.Vector2(self.rect.center)
         self.vec_bottomleft = pygame.Vector2(self.rect.bottomleft) - pygame.Vector2(self.rect.center)
         self.vec_topleft = pygame.Vector2(self.rect.topleft) - pygame.Vector2(self.rect.center)
 
     def render(self):
-        pygame.draw.rect(screen, (255, 160, 122), self.rect)
-
-
-
-def liesbetween(a: pygame.Vector2, b: pygame.Vector2, c: pygame.Vector2):
-    return a.cross(b) * a.cross(c) >= 0 and c.cross(b) * c.cross(a) >= 0
+        pygame.draw.rect(SCREEN, (255, 160, 122), self.rect)
 
 
 class Ball:
     def __init__(self):
-        self.ball_r = 20
-        self.rect = pygame.Rect(screen_w // 2 - self.ball_r // 2, screen_h - self.ball_r - plblock.plblock_h - 60,
-                                self.ball_r, self.ball_r)
+        self.radius = 20
+        self.rect = pygame.Rect(
+            (SCREEN_W // 2 - self.radius // 2, SCREEN_H - self.radius - plblock.plblock_h - 60),
+            (self.radius, self.radius)
+        )
         self.speed = 5
         self.dir_x = 1
         self.dir_y = -1
@@ -73,33 +62,37 @@ class Ball:
         # шарик и экран
         self.rect.x += self.speed * self.dir_x
         self.rect.y += self.speed * self.dir_y
-        if screen_w - self.ball_r < self.rect.x or self.rect.x < 0:
-            self.dir_x = - self.dir_x
-        if screen_h - self.ball_r < self.rect.y or self.rect.y < 0:
-            self.dir_y = - self.dir_y
+        if SCREEN_W - self.radius < self.rect.x or self.rect.x < 0:
+            self.dir_x *= -1
+        if SCREEN_H - self.radius < self.rect.y or self.rect.y < 0:
+            self.dir_y *= -1
         # шарик и игровая платформа
         if self.rect.colliderect(plblock.rect) and self.dir_y > 0:
-            self.dir_y = - self.dir_y
+            self.dir_y *= -1
         # ВЗАИМОДЕЙСТВИЕ ШАРИКА И ПЛАТФОРМ
         for platform in platforms:
-            # вектор от центра платформы до центра шарика
-            vector = pygame.Vector2(self.rect.center) - pygame.Vector2(platform.rect.center)
             # проверка с какой стороны платформы произошло столкновение
             if self.rect.colliderect(platform.rect):
-                if liesbetween(platform.vec_topright, vector, platform.vec_bottomright):  # справа
+                def lies_between(a: pygame.Vector2, b: pygame.Vector2, c: pygame.Vector2):
+                    return a.cross(b) * a.cross(c) >= 0 and c.cross(b) * c.cross(a) >= 0
+
+                # вектор от центра платформы до центра шарика
+                vector = pygame.Vector2(self.rect.center) - pygame.Vector2(platform.rect.center)
+                if lies_between(platform.vec_topright, vector, platform.vec_bottomright):  # справа
                     ball.dir_x = - ball.dir_x
-                if liesbetween(platform.vec_topright, vector, platform.vec_topleft):  # сверху
+                if lies_between(platform.vec_topright, vector, platform.vec_topleft):  # сверху
                     ball.dir_y = - ball.dir_y
-                if liesbetween(platform.vec_topleft, vector, platform.vec_bottomleft):  # слева
+                if lies_between(platform.vec_topleft, vector, platform.vec_bottomleft):  # слева
                     ball.dir_x = - ball.dir_x
-                if liesbetween(platform.vec_bottomleft, vector, platform.vec_bottomright):  # снизу
+                if lies_between(platform.vec_bottomleft, vector, platform.vec_bottomright):  # снизу
                     ball.dir_y = - ball.dir_y
 
                 platforms.remove(platform)
                 game.score += 1
 
     def render(self):
-        pygame.draw.rect(screen, (178, 34, 34), self.rect)
+        self.update()
+        pygame.draw.rect(SCREEN, (178, 34, 34), self.rect)
 
 
 class Game:
@@ -108,24 +101,22 @@ class Game:
         self.score = 0
 
     def draw_lives(self):
-        lives_t = font.render(f"Lives: {self.lives}", True, (255, 255, 255))
-        screen.blit(lives_t, (10, 10))
+        lives_t = FONT.render(f"Lives: {self.lives}", True, (255, 255, 255))
+        SCREEN.blit(lives_t, (10, 10))
 
     def draw_score(self):
-        score_t = font.render(f"Score: {self.score}", True, (255, 255, 255))
-        screen.blit(score_t, (screen_w - 80, 10))
+        score_t = FONT.render(f"Score: {self.score}", True, (255, 255, 255))
+        SCREEN.blit(score_t, (SCREEN_W - 80, 10))
 
-    def gameover(self) :
-        if ball.rect.y + ball.ball_r >= screen_h:
+    def process_game_over(self):
+        if ball.rect.y + ball.radius >= SCREEN_H:
             self.lives -= 1
             ball.__init__()
 
         if self.lives <= 0:
-            screen.blit(game_over_text, (screen_w // 2 - 100, screen_h // 2))
-            pygame.display.update()
-            pygame.time.wait(2000)
-            pygame.quit()
-
+            SCREEN.blit(game_over_text, (SCREEN_W // 2 - 100, SCREEN_H // 2))
+            global GAME_STATE
+            GAME_STATE = "GAME_OVER"
 
 
 # состояние игры
@@ -136,28 +127,25 @@ play_button = None
 
 # объявление переменных игры
 game = Game()
-ball = Ball()
 plblock = Plblock()
+ball = Ball()
 platforms = []
-for x in range(25, screen_w - 65, 75):
-    for y in range(30, screen_h // 2, 35):
+for x in range(25, SCREEN_W - 65, 75):
+    for y in range(30, SCREEN_H // 2, 35):
         platforms.append(Platform(x, y))
 time = 400
 
-
 # объявление переменных экрана окончания игры
-game_over_text = font.render("Game Over", True, (255, 0, 0))
-
+game_over_text = FONT.render("Game Over", True, (255, 0, 0))
 
 # ЗАДАЕМ МОМЕНТ ВЫКЛЮЧЕНИЯ ПРОГРАМЫ
 while True:
-    for e1 in pygame.event.get():
-        if e1.type == pygame.QUIT:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            break
 
-    screen.fill((0, 0, 0))
-
+    SCREEN.fill((0, 0, 0))
     if GAME_STATE == "MENU":
         pass
     elif GAME_STATE == "GAME":
@@ -165,30 +153,24 @@ while True:
     elif GAME_STATE == "MENU":
         pass
 
-    time = time - 1
+    time -= 1
     if time == 0:
         for platform in platforms:
-            platform.rect.y = platform.rect.y + 35
-        for x in range(25, screen_w - 65, 75):
+            platform.rect.y += 35
+        for x in range(25, SCREEN_W - 65, 75):
             platforms.append(Platform(x, 30))
         time = 400
 
-    # screen.blit(image, (0,0))
-
-
-    plblock.update()
     plblock.render()
 
     for platform in platforms:
         platform.render()
 
-    ball.update()
     ball.render()
 
     game.draw_score()
     game.draw_lives()
-    game.gameover()
+    game.process_game_over()
 
     pygame.display.flip()
-
-    clock.tick(60)
+    CLOCK.tick(60)
